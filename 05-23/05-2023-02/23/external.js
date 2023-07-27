9b59dafa00568efb00162576dacf08fd4e380701
S Rudra Prasad Choudhury <98145990+srpchoudhury@users.noreply.github.com>
2023-07-27 13:11:30 +0530

    commit

---
+++ 05-23/05-2023-02/23/external.js
@@ -0,0 +1,39 @@
+function add(){
+    console.log(a);
+    var x=20;
+    var y=30;
+   var a;
+//    console.log(a);
+} 
+
+function sub(){
+    console.log(a);
+    let x=20;
+    let y=30;
+//    let a=x-y;
+   console.log(a);
+}
+
+function mul(){
+    console.log(a);
+    const x=20;
+    const y=30;
+//    const a=x*y;
+   console.log(a);
+}
+
+// function add(){
+//     var a=20;
+//     var b=30;
+//     alert(a+b);
+// }
+// function sub(){
+//     let a=40;
+//     let b=30;
+//     alert(a-b);
+// }
+// function mul(){
+//     const a=40;
+//     const b=4;
+//     alert(a*b);
+// }
